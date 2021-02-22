607bddaa17b22721ceda619691e8308264efd1e3
Sergiy Naumov <1414sergiy@gmail.com>
2021-02-22 19:10:05 +0200

    Parses name, age and title which the applicant applies for

--- scrapy_workua/scrapy_workua/spiders/workua.py
+++ scrapy_workua/scrapy_workua/spiders/workua.py
@@ -4,7 +4,19 @@ import scrapy
 class WorkuaSpider(scrapy.Spider):
     name = 'workua'
     allowed_domains = ['work.ua']
-    start_urls = ['http://work.ua/']
+    start_urls = [
+        'https://www.work.ua/resumes-kharkiv/',
+    ]
 
     def parse(self, response):
-        pass
+        # A loop is created to parse specific data.
+        for person in response.css('div.card.resume-link'):
+            applicant_name = person.css('div > b::text').get()
+            applicant_old = person.css('div > span:nth-child(3)::text').get()
+            applicant_position = person.css('h2 > a::text').get()
+
+            yield {
+                "applicant_name": applicant_name,
+                "applicant_old": applicant_old,
+                "applicant_position": applicant_position,
+            }
